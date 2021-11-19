6e2f5252e4507592fea679cd5f640beea6b4920d
lishu <lishugupta652@gmail.com>
2021-11-19 21:39:35 +0530

    home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <div className="header">
+          <div className="logo">
+            lishu <br /> gupta
+          </div>
+          <ul>
+            <li>
+              <Link className="link" to="">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className="link" to="/about">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link className="link" to="/contact">
+                Contact
+              </Link>
+            </li>
+          </ul>
+          <Routes>
+            <Route exactpath="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/contact" component={Contact} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="home-image home-overlay"></div>
+      <div className="container-home">
+        <h1>homepage</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
