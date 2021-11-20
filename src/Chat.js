ecdf04a72fa529e6cfdd3530f10363593dd7bb2f
lishu <lishugupta652@gmail.com>
2021-11-20 22:11:21 +0530

    Todo: chatBody

--- src/Chat.js
+++ src/Chat.js
@@ -1,7 +1,36 @@
+import { AttachFile, SearchOutlined } from "@mui/icons-material";
+import MoreVert from "@mui/icons-material/MoreVert";
+import { Avatar, IconButton } from "@mui/material";
 import React from "react";
 
 const Chat = () => {
-  return <div className="chat">Chat</div>;
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar />
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>last seen at..</p>
+        </div>
+
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="chat__body">
+        <p>Rhisis message</p>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -5,7 +5,7 @@ const SidebarChat = () => {
   return (
     <div className="sidebarChat">
       <Avatar />
-      <div className="sidebarChat_info">
+      <div className="sidebarChat__info">
         <h2>Room name</h2>
         <p>This is the last message</p>
       </div>
