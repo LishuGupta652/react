5767d9b1f276dd07596a722e02b2e3291dd1530a
lishu <lishugupta652@gmail.com>
2021-11-20 21:50:02 +0530

    sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,34 @@
 import React from "react";
-
+import { Avatar, IconButton } from "@mui/material";
+import DonutLargeIcon from "@mui/icons-material/DonutLarge";
+import ChatIcon from "@mui/icons-material/Chat";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { SearchOutlined } from "@mui/icons-material";
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <div className="sidebar__header"></div>
+      <div className="sidebar__header">
+        <Avatar src="https://avatars.dicebear.com/api/human/112.svg" />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input type="text" placeholder="Search or Start a new Chat... " />
+        </div>
+      </div>
+
+      <div className="sidebar__chat"></div>
     </div>
   );
 };
